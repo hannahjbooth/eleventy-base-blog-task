f355b562c7807033b393c0f415181f328245c4d1
Hannah Booth <hannahjbooth9@gmail.com>
2024-05-22 12:05:19 +0100

    edited blog's information within _data/metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
+	title: "Hannah's Eleventy Base Blog Task",
 	url: "https://example.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "This is my task submission for the SSG session.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
+		name: "Hannah Booth",
+		email: "hannahjbooth9@gmail.com",
 		url: "https://example.com/about-me/"
 	}
 }
